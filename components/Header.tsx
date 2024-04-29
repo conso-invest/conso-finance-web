4c1a8bc567e27285554736c76438ddcfd959405b
winorg394 <winorg68@gmail.com>
2024-04-29 08:52:21 +0100

    authentification update

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,60 +2,107 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
-import logo from '../public/logo.png';
+import logo from "../public/logo.png";
 import { SearchIcon, UserRound, MenuIcon } from "lucide-react";
+import { UserData } from "@/lib/const";
 
 function Header() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [userData, setUserData] = useState<any>();
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <header className="fixed w-full z-10 shadow-md bg-white px-5 py-2">
-            <div className="flex justify-between items-center h-16">
-                <div className="flex items-center space-x-4">
-                    <button onClick={toggleMenu} className="text-gray-600 focus:outline-none lg:hidden">
-                        <MenuIcon size={24} />
-                    </button>
-                    <Link href="/" className="hidden lg:flex">
-                        <Image src={logo} width={70} height={10} alt="logo" className="cursor-pointer py-2" />
-                    </Link>
+  const isConnected = () => {
+    const userData = localStorage.getItem("UserData");
+    return userData !== null;
+  };
 
-                    <Link href="/" className="hidden lg:flex text-xs font-bold hover:text-blue-400">LANCER UN PROJET</Link>
-                    <Link href="/" className="hidden lg:flex text-xs font-bold hover:text-blue-400">
-                        PARTICIPER
-                    </Link>
-                </div>
-                <Link href="/" className="ml-8 lg:hidden">
-                    <Image src={logo}  alt="logo" className="w-16 cursor-pointer py-2" />
-                </Link>
-                <div className="flex items-center space-x-6">
-                    <Link href="/search" className="text-xs font-bold hover:text-blue-400 flex items-center">
-                        <SearchIcon width={20} className="lg:mr-2" />
-                        <span className="hidden lg:flex"> RECHERCHER UN PROJET</span>
-                    </Link>
-                    <div className="hidden lg:flex h-10 border-r border-gray-300"></div>
-                    <Link href="/login" className="text-xs font-bold hover:text-blue-400 flex items-center">
-                        <UserRound width={20} className="lg:mr-2" />
-                        <span className="hidden lg:flex">SE CONNECTER</span>
-                    </Link>
-                </div>
-            </div>
+  const getUserName = () => {
+    const userData = localStorage.getItem("UserData");
+    return userData ? JSON.parse(userData).name : null;
+  };
 
-            {
-                isMenuOpen && (
-                    <div className="lg:hidden h-screen bg-white absolute pt-5 left-0 right-0 shadow-md">
-                        <div className="px-4 py-2">
-                            <Link href="/" className="block py-2 border-b border-gray-200">LANCER UN PROJET</Link>
-                            <Link href="/" className="block py-2">PARTICIPER</Link>
-                        </div>
-                    </div>
-                )
-            }
-        </header >
-    );
+  return (
+    <header className="fixed z-10 w-full px-5 py-2 bg-white shadow-md">
+      <div className="flex items-center justify-between h-16">
+        <div className="flex items-center space-x-4">
+          <button
+            onClick={toggleMenu}
+            className="text-gray-600 focus:outline-none lg:hidden"
+          >
+            <MenuIcon size={24} />
+          </button>
+          <Link href="/" className="hidden lg:flex">
+            <Image
+              src={logo}
+              width={70}
+              height={10}
+              alt="logo"
+              className="py-2 cursor-pointer"
+            />
+          </Link>
+
+          <Link
+            href="/"
+            className="hidden text-xs font-bold lg:flex hover:text-blue-400"
+          >
+            LANCER UN PROJET
+          </Link>
+          <Link
+            href="/"
+            className="hidden text-xs font-bold lg:flex hover:text-blue-400"
+          >
+            PARTICIPER
+          </Link>
+        </div>
+        <Link href="/" className="ml-8 lg:hidden">
+          <Image src={logo} alt="logo" className="w-16 py-2 cursor-pointer" />
+        </Link>
+        <div className="flex items-center space-x-6">
+          <Link
+            href="/search"
+            className="flex items-center text-xs font-bold hover:text-blue-400"
+          >
+            <SearchIcon width={20} className="lg:mr-2" />
+            <span className="hidden lg:flex"> RECHERCHER UN PROJET</span>
+          </Link>
+          <div className="hidden h-10 border-r border-gray-300 lg:flex"></div>
+          {isConnected() ? (
+            <Link
+              href="/dashboard"
+              className="flex items-center text-xs font-bold hover:text-blue-400"
+            >
+              <UserRound width={20} className="lg:mr-2" />
+              <span className="hidden lg:flex">{getUserName()}</span>
+            </Link>
+          ) : (
+            <Link
+              href="/login"
+              className="flex items-center text-xs font-bold hover:text-blue-400"
+            >
+              <UserRound width={20} className="lg:mr-2" />
+              <span className="hidden lg:flex">SE CONNECTER</span>
+            </Link>
+          )}
+        </div>
+      </div>
+
+      {isMenuOpen && (
+        <div className="absolute left-0 right-0 h-screen pt-5 bg-white shadow-md lg:hidden">
+          <div className="px-4 py-2">
+            <Link href="/" className="block py-2 border-b border-gray-200">
+              LANCER UN PROJET
+            </Link>
+            <Link href="/" className="block py-2">
+              PARTICIPER
+            </Link>
+          </div>
+        </div>
+      )}
+    </header>
+  );
 }
 
 export default Header;
---
+++ lib/apiEndpoints.ts
@@ -0,0 +1,34 @@
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+export const auth = {
+  login: `${apiUrl}/auth/login`,
+  register: `${apiUrl}/auth/register`,
+  logout: `${apiUrl}/auth/logout`,
+  refresh: `${apiUrl}/auth/refresh`,
+};
+export const profil = {
+  list: `${apiUrl}/profil/list`,
+};
+/* export const users = {
+  list: `${apiUrl}/users`,
+  create: `${apiUrl}/users`,
+  get: (id) => `${apiUrl}/users/${id}`,
+  update: (id) => `${apiUrl}/users/${id}`,
+  delete: (id) => `${apiUrl}/users/${id}`,
+};
+
+export const products = {
+  list: `${apiUrl}/products`,
+  create: `${apiUrl}/products`,
+  get: (id) => `${apiUrl}/products/${id}`,
+  update: (id) => `${apiUrl}/products/${id}`,
+  delete: (id) => `${apiUrl}/products/${id}`,
+};
+
+export const orders = {
+  list: `${apiUrl}/orders`,
+  create: `${apiUrl}/orders`,
+  get: (id) => `${apiUrl}/orders/${id}`,
+  update: (id) => `${apiUrl}/orders/${id}`,
+  delete: (id) => `${apiUrl}/orders/${id}`,
+}; */
