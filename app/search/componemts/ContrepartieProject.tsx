a4677268f35ccc337fdeb718e5aa485256c39943
innov237 <innov237@gmail.com>
2024-05-07 15:35:11 +0100

    update

--- app/search/componemts/ContrepartieProject.tsx
+++ app/search/componemts/ContrepartieProject.tsx
@@ -10,8 +10,13 @@ const ContrepartieProject = ({ item }: any) => {
       {item?.length > 0 &&
         <div className="flex">
           <div>
+            <div className="mb-10">
+              <h1 className="text-bold text-2xl mt-10">Choisissez vos contreparties</h1>
+              <p className="mt-1">Soutenez la collecte et recevez des contreparties en échange.</p>
+            </div>
+
             {item.map((item: any) =>
-              <div className="w-3/6 shadow-sm mb-10 flex items-center border-b border-gray-200 p-4 rounded-lg" key={item.id}>
+              <div className="w-3/6 border border-secondarycolor shadow-sm mb-10 flex items-center border-b border-gray-200 p-4 rounded-lg" key={item.id}>
                 <div className="w-full">
                   <div className="flex justify-between mb-7">
                     <div className="text-xl text-gray-600 font-bold">Pour {item.montant}Fcfa</div>
