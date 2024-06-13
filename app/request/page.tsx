9292b65b7877b02b5d1232124a6026096cbfdb1e
innov237 <innov237@gmail.com>
2024-06-13 20:45:37 +0100

    update request

--- app/request/page.tsx
+++ app/request/page.tsx
@@ -3,163 +3,96 @@ import axios from "axios";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select } from "@/components/ui/select";
-import { ChangeEvent, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
 import { auth, cat, profil, project } from "@/lib/apiEndpoints";
 import { UserData } from "@/lib/const";
-import { BookAIcon, ChefHatIcon, SunIcon, UsersIcon } from "lucide-react";
+import { BookAIcon, ChefHatIcon, SunIcon, UsersIcon } from "lucide-react"
+import Modal from "@/components/ui/modal";
 
 function PublishRequestPage() {
-  const [titre, setTitre] = useState("");
-
-  const [sousTitre, setSousTitre] = useState("");
+  const { register, handleSubmit, reset, setValue, watch, formState: { errors } } = useForm();
   const [image, setImage] = useState<any>(null);
-  const [objectif, setObjectif] = useState("");
-  const [description, setDescription] = useState("");
-  const [idCategorie, setIdCategorie] = useState("");
   const [error, setError] = useState("");
   const [preview, setPreview] = useState<any>();
   const [category, setCategory] = useState<any>([]);
   const [user, setUser] = useState<any>({});
   const [isLoad, setIsLoad] = useState(false);
   const [isLoadDetail, setIsLoadDetail] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  
 
   function getQueryParam(param: string): string {
-    // Create a new URL object from the current URL
     const urlObj: URL = new URL(window.location.href);
-
-    // Get the search parameters from the URL
     const params: URLSearchParams = new URLSearchParams(urlObj.search);
-
-    // Get the value of the specified parameter
     const value: string | null = params.get(param);
-
-    // Check if the parameter is null and handle it
-    if (value === null) {
-      return "";
-    }
-
-    return value;
+    return value === null ? "" : value;
   }
-
 
   const getDataCurrent = async (id: any) => {
     try {
-
       setIsLoadDetail(true);
-
       const response = await axios.post(project.get_curent_request(id));
-      console.log(response.data);
       if (response.data.success) {
         setIsLoadDetail(false);
-
-        setTitre(response.data.data.titre);
-        setSousTitre(response.data.data.sous_titre);
-
-        setObjectif(response.data.data.objectif);
-        setDescription(response.data.data.description);
-        setIdCategorie(response.data.data.id_categorie);
-        //   setError("");
+        setValue("titre", response.data.data.titre);
+        setValue("sousTitre", response.data.data.sous_titre);
+        setValue("objectif", response.data.data.objectif);
+        setValue("description", response.data.data.description);
+        setValue("idCategorie", response.data.data.id_categorie);
         setPreview(response.data.data.image);
-      } else {
-        // window.location.href = "/request";
       }
-      //window.location.href = "/";
-      // Redirect to a success page or display a success message
-      // setError("");
-      //  setImage<File | null>(null);
-
-      /*       setTitre("");
-            setSousTitre("");
-        
-            setObjectif("");
-            setDescription("");
-            setIdCategorie("");
-            setError("");
-            setPreview<string | null>(null); */
     } catch (error) {
-      console.error(error);
-
       setIsLoadDetail(false);
-
+      console.error(error);
     }
   }
 
-  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.files) {
-      const file = event.target.files[0];
-      if (file) {
-        if (file.size > 1000000) {
-          setError(
-            "Le fichier est trop volumineux. Veuillez choisir un fichier de taille inférieure à 1 Mo."
-          );
-          return;
-        }
-        const fileUrl = URL.createObjectURL(file);
-        setPreview(fileUrl);
-        setImage(file);
-        setError("");
+  const handleImageChange = (event: any) => {
+    const file = event.target.files[0];
+    if (file) {
+      if (file.size > 1000000) {
+        setError("Le fichier est trop volumineux. Veuillez choisir un fichier de taille inférieure à 1 Mo.");
+        return;
       }
+      const fileUrl = URL.createObjectURL(file);
+      setPreview(fileUrl);
+      setImage(file);
+      setError("");
     }
   };
 
-  const handleSubmit = async (event: ChangeEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (
-      !titre ||
-      !objectif ||
-      !description ||
-      !idCategorie
-    ) {
+  const onSubmit = async (data: any) => {
+    if (!data.titre || !data.objectif || !data.description || !data.idCategorie) {
       setError("Veuillez remplir tous les champs.");
       return;
     }
 
     try {
-
       setIsLoad(true);
-
       const formData = new FormData();
-      formData.append("titre", titre);
-      formData.append("sous_titre", sousTitre);
-
-
-      if (image && getQueryParam('id') == null) {
-        formData.append("image", image as Blob);
-      } else if (image == null && getQueryParam('id') != null) {
-        formData.append("image", preview);
-      } else if (image != null && getQueryParam('id') != null) {
-        formData.append("image", image as Blob);
-      } else if (image != null && getQueryParam('id') == null) {
-        formData.append("image", image as Blob);
-      } else {
+      formData.append("titre", data.titre);
+      formData.append("sous_titre", data.sousTitre);
+      if (image) {
         formData.append("image", image as Blob);
       }
-
-
-      formData.append("objectif", objectif);
-      formData.append("description", description);
-      formData.append("id_categorie", idCategorie);
+      formData.append("objectif", data.objectif);
+      formData.append("description", data.description);
+      formData.append("id_categorie", data.idCategorie);
       formData.append("token", user.token);
       formData.append("user_id", user.id);
 
-      console.log(getQueryParam('id') == null ? project.send_request : project.send_request + "/" + getQueryParam('id'));
-
-      const response = await axios.post(getQueryParam('id') == null ? project.send_request : project.send_request + "/" + getQueryParam('id'), formData);
-
+      const response = await axios.post(project.send_request, formData);
       setIsLoad(false);
-      // window.location.href = "/";
-      if (getQueryParam('id') !== null) {
-        getDataCurrent(getQueryParam('id'));
-
-      }
-      // Redirect to a success page or display a success message
       setError("");
+      reset(); // Reset the form
+      setPreview(null); // Clear the image preview
+      setImage(null); // Clear the image state
+      setShowModal(true);
     } catch (error) {
       console.error(error);
       setIsLoad(false);
-      setError(
-        "Une erreur est survenue lors de l'envoi de la demande. Veuillez réessayer."
-      );
+      setError("Une erreur est survenue lors de l'envoi de la demande. Veuillez réessayer.");
     }
   };
 
@@ -167,37 +100,24 @@ function PublishRequestPage() {
     try {
       const response = await axios.get(project.getAllCategory);
       setCategory(response.data.data);
-      setIdCategorie(response.data.data[0]?.id);
+      setValue("idCategorie", response.data.data[0]?.id);
     } catch (error) {
       console.error(error);
     }
   };
 
   const checkIfIsConnected = () => {
-
     const userData = localStorage.getItem(UserData) || '';
-
     if (userData != '') {
       setUser(JSON.parse(userData));
     } else {
       window.location.href = "/login";
     }
-
   };
 
   const init = () => {
-
-    try {
-
-      if (getQueryParam('id') !== null) {
-        getDataCurrent(getQueryParam('id'));
-      }
-    } catch (error) {
-
-      if (getQueryParam('id') == null) {
-        // setIsLoadDetail(false);
-      }
-      console.error(error);
+    if (getQueryParam('id')) {
+      getDataCurrent(getQueryParam('id'));
     }
   }
 
@@ -205,44 +125,43 @@ function PublishRequestPage() {
     checkIfIsConnected();
     fetchCategory();
     init();
-
   }, []);
 
   return (
     <div className="banner px-4 py-12 pt-28 lg:py-20 lg:px-24 lg:pt-36">
       <div className="w-full flex flex-col justify-center items-center mb-20">
         <p className="text-2xl lg:text-6xl font-extrabold text-center text-primarycolor">
-          Lancez-vous sur  consofinance
+          Lancez-vous sur consofinance
         </p>
         <h1 className="text-md mb-2 lg:mb-5 my-4 lg:text-xl">{`Prenez quelques minutes pour le décrire afin qu'on puisse vous accompagner au mieux`}</h1>
       </div>
 
       <div className="w-full">
         <div className="space-x-2 lg:flex justify-between lg:space-x-10">
-          <form onSubmit={isLoad ? () => null : handleSubmit} className="p-8 bg-white shadow-lg lg:p-10">
+          <form onSubmit={handleSubmit(onSubmit)} className="p-8 bg-white shadow-lg lg:p-10">
             <div>
               <label htmlFor="titre" className="font-bold">
                 Que voulez-vous financer ?
               </label>
               <Input
+                {...register("titre", { required: true })}
                 placeholder=""
                 type="text"
                 className="h-12"
-                value={titre}
-                onChange={(event) => setTitre(event.target.value)}
               />
+              {errors.titre && <p className="text-red-500">Ce champ est requis</p>}
             </div>
             <div className="mt-8">
-              <label htmlFor="sous_titre" className="font-bold">
+              <label htmlFor="sousTitre" className="font-bold">
                 Nom du projet / Entreprise
               </label>
               <Input
+                {...register("sousTitre", { required: true })}
                 placeholder=""
                 type="text"
                 className="h-12"
-                value={sousTitre}
-                onChange={(event) => setSousTitre(event.target.value)}
               />
+               {errors.sousTitre && <p className="text-red-500">Ce champ est requis</p>}
             </div>
             <div className="mt-8">
               <label htmlFor="image" className="font-bold mb-2">
@@ -250,7 +169,6 @@ function PublishRequestPage() {
               </label>
               <div className="flex flex-col items-center justify-center lg:p-2 border-2 border-dashed border-gray-300 rounded-md">
                 <Input
-                  placeholder=""
                   type="file"
                   className="sr-only" id="file-input" onChange={handleImageChange}
                 />
@@ -277,9 +195,9 @@ function PublishRequestPage() {
                         xmlns="http://www.w3.org/2000/svg"
                       >
                         <path
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          stroke-width="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
                           d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
                         ></path>
                       </svg>
@@ -289,7 +207,6 @@ function PublishRequestPage() {
                       </p>
                     </div>
                   )}
-
                 </label>
               </div>
             </div>
@@ -298,30 +215,29 @@ function PublishRequestPage() {
                 Budget souhaité
               </label>
               <Input
+                {...register("objectif", { required: true })}
                 placeholder=""
                 type="number"
                 className="h-12"
-                value={objectif}
-                onChange={(event) => setObjectif(event.target.value)}
               />
+              {errors.objectif && <p className="text-red-500">Ce champ est requis</p>}
             </div>
             <div className="mt-8">
               <label htmlFor="description" className="font-bold">
                 Palez nous de vous et du projet
               </label>
               <textarea
+                {...register("description", { required: true })}
                 className="p-2 w-full h-32 mt-2 border rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 shadow-sm"
-                value={description}
-                onChange={(event) => setDescription(event.target.value)}
               />
+              {errors.description && <p className="text-red-500">Ce champ est requis</p>}
             </div>
             <div className="mt-8">
-              <label htmlFor="id_categorie" className="font-bold">
+              <label htmlFor="idCategorie" className="font-bold">
                 Catégorie
               </label>
               <Select
-                value={idCategorie}
-                onChange={(event) => setIdCategorie(event.target.value)}
+                {...register("idCategorie", { required: true })}
               >
                 {category.map((option: any) => (
                   <option key={option.id} value={option.id}>
@@ -329,11 +245,12 @@ function PublishRequestPage() {
                   </option>
                 ))}
               </Select>
+              {errors.idCategorie && <p className="text-red-500">Ce champ est requis</p>}
             </div>
             {error && <p className="text-red-500 mt-5">{error}</p>}
             <div className="mt-4">
               <Button className="w-full h-12 text-xl bg-secondarycolor">
-                {isLoad ? "Traitement..." : " Envoyer ma demande"}
+                {isLoad ? "Traitement..." : "Envoyer ma demande"}
               </Button>
             </div>
           </form>
@@ -376,10 +293,23 @@ function PublishRequestPage() {
               </div>
             </div>
           </div>
-
         </div>
       </div>
 
+      {showModal && <Modal
+        id="default-modal"
+        title="Demande envoyée"
+        content={[
+          "Votre demande a été envoyée avec succès. Nous l'étudierons avec soin et vous contacterons dans les plus brefs délais. Consofinance vous remercie de votre confiance",
+        ]}
+        acceptBtn={() => {
+          setShowModal(false);
+        }}
+        declineBtn={() => {
+          setShowModal(false);
+        }}
+      />
+      }
     </div>
   );
 };
--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -1,4 +1,3 @@
-import { Trash2Icon } from 'lucide-react';
 import React from 'react';
 
 // Définir explicitement le type de la prop "id"
@@ -13,35 +12,37 @@ interface ModalProps {
 const Modal: React.FC<ModalProps> = ({ id, title, content, acceptBtn, declineBtn }) => {
     return (
         <>
-            <div className="p-4 justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
-                <div className="relative w-auto my-6 mx-auto max-w-3xl">
+            <div className="p-5 fixed inset-0 z-50 flex items-center justify-center overflow-x-hidden overflow-y-auto outline-none focus:outline-none">
+                <div className="relative w-auto max-w-3xl mx-auto my-6">
                     {/*content*/}
                     <div className="bg-primarycolor border-0 rounded-lg shadow-lg relative flex flex-col w-full outline-none focus:outline-none">
                         {/*header*/}
                         <div className="flex items-start justify-between p-5 border-b border-solid border-secondarycolor rounded-t">
                             <h3 className="text-xl lg:text-3xl font-semibold text-secondarycolor">{title}</h3>
                             <button
-                                className="p-1 ml-auto  border-0 text-black opacity- float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                                className="ml-auto text-black bg-transparent border-0 text-3xl leading-none font-semibold outline-none focus:outline-none"
                                 onClick={declineBtn}
                             >
-                                <span className="text-black">
-                                    X
-                                </span>
+                                <span className="text-black opacity-75 hover:opacity-100">×</span>
                             </button>
                         </div>
                         {/*body*/}
-                        <p className='text-white text-xl p-4 lg:px-4 lg:py-20 text-center'>{content}</p>
+                        <div className="p-4 lg:px-4 lg:py-20 text-center">
+                            {content.map((text, index) => (
+                                <p key={index} className="text-white text-xl mb-4">{text}</p>
+                            ))}
+                        </div>
                         {/*footer*/}
                         <div className="flex items-center justify-end p-6 border-t border-solid border-secondarycolor rounded-b">
                             <button
-                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                className="text-red-500 bg-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-red-500 hover:text-white"
                                 type="button"
                                 onClick={declineBtn}
                             >
-                                Annuler
+                                Fermer
                             </button>
                             <button
-                                className="bg-secondarycolor text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                className="bg-secondarycolor text-white active:bg-secondarycolor font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150 hover:bg-opacity-90"
                                 type="button"
                                 onClick={acceptBtn}
                             >
@@ -51,7 +52,7 @@ const Modal: React.FC<ModalProps> = ({ id, title, content, acceptBtn, declineBtn
                     </div>
                 </div>
             </div>
-            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+            <div className="fixed inset-0 z-40 bg-black opacity-50"></div>
         </>
     );
 };
